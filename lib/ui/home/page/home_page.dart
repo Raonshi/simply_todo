e7deeee6766531ffebc785dd0ea1afd37066777e
Raonshi <sunwonsw95@naver.com>
2024-08-07 20:32:29 +0900

    feat:add event calendar ui

--- lib/ui/home/page/home_page.dart
+++ lib/ui/home/page/home_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:simpletodo/bloc/todo_list/todo_list_bloc.dart';
 import 'package:simpletodo/common/theme.dart';
 import 'package:simpletodo/domain/repository/todo/todo_repository_impl.dart';
 import 'package:simpletodo/ui/add_todo/page/add_todo_page.dart';
+import 'package:simpletodo/ui/home/widget/todo_calendar.dart';
 import 'package:simpletodo/ui/home/widget/todo_panel.dart';
 
 class HomePage extends StatelessWidget {
@@ -24,8 +26,23 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class _HomePageBody extends StatelessWidget {
+class _HomePageBody extends StatefulWidget {
   const _HomePageBody();
+
+  @override
+  State<_HomePageBody> createState() => _HomePageBodyState();
+}
+
+class _HomePageBodyState extends State<_HomePageBody> {
+  late int _currentIndex;
+  late final PageController _pageController;
+
+  @override
+  void initState() {
+    _currentIndex = 0;
+    _pageController = PageController(initialPage: _currentIndex);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,60 +56,127 @@ class _HomePageBody extends StatelessWidget {
           style: context.textTheme.displayMedium,
         ),
       ),
-      body: BlocBuilder<TodoListBloc, TodoListState>(
-        builder: (context, state) => switch (state) {
-          TodoListInitial _ => Container(),
-          TodoListLoading _ => const Center(
-              child: CircularProgressIndicator.adaptive(),
-            ),
-          TodoListLoaded loaded => TodoPanel(
-              todos: loaded.todos,
-              onRefresh: context.read<TodoListBloc>().refresh,
-            ),
-          TodoListError error => Center(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: context.colorTheme.error,
-                  borderRadius: BorderRadius.circular(8.0),
+      body: PageView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: _pageController,
+        children: [
+          BlocBuilder<TodoListBloc, TodoListState>(
+            builder: (context, state) => switch (state) {
+              TodoListInitial _ => Container(),
+              TodoListLoading _ => const Center(
+                  child: CircularProgressIndicator.adaptive(),
                 ),
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 40.0, vertical: 12.0),
-                child: Text(
-                  error.exception.toString(),
-                  style: context.textTheme.labelLarge?.copyWith(
-                    color: context.colorTheme.onError,
+              TodoListLoaded loaded => TodoPanel(
+                  todos: loaded.todos,
+                  onRefresh: context.read<TodoListBloc>().refresh,
+                ),
+              TodoListError error => Center(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: context.colorTheme.error,
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 40.0, vertical: 12.0),
+                    child: Text(
+                      error.exception.toString(),
+                      style: context.textTheme.labelLarge?.copyWith(
+                        color: context.colorTheme.onError,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-          // ignore: unreachable_switch_case
-          _ => Container(),
-        },
+              // ignore: unreachable_switch_case
+              _ => Container(),
+            },
+          ),
+          BlocBuilder<TodoListBloc, TodoListState>(
+            builder: (context, state) => switch (state) {
+              TodoListInitial _ => Container(),
+              TodoListLoading _ => const Center(
+                  child: CircularProgressIndicator.adaptive(),
+                ),
+              TodoListLoaded loaded => TodoCalendar(
+                  todos: loaded.todos,
+                ),
+              TodoListError error => Center(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: context.colorTheme.error,
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 40.0,
+                      vertical: 12.0,
+                    ),
+                    child: Text(
+                      error.exception.toString(),
+                      style: context.textTheme.labelLarge?.copyWith(
+                        color: context.colorTheme.onError,
+                      ),
+                    ),
+                  ),
+                ),
+              // ignore: unreachable_switch_case
+              _ => Container(),
+            },
+          ),
+        ],
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: Row(
-          children: [
-            Expanded(
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: context.colorTheme.onPrimary,
-                  foregroundColor: context.colorTheme.primary,
-                  textStyle: context.textTheme.titleSmall?.copyWith(
-                    color: context.colorTheme.primary,
-                  ),
-                ),
-                onPressed: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const AddTodoPage()),
-                ).then((_) => context.read<TodoListBloc>().refresh()),
-                child: const Text("일정 추가"),
+      floatingActionButton: _currentIndex == 0
+          ? Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: context.colorTheme.onPrimary,
+                        foregroundColor: context.colorTheme.primary,
+                        textStyle: context.textTheme.titleSmall?.copyWith(
+                          color: context.colorTheme.primary,
+                        ),
+                      ),
+                      onPressed: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const AddTodoPage()),
+                      ).then((_) => context.read<TodoListBloc>().refresh()),
+                      child: const Text("일정 추가"),
+                    ),
+                  )
+                ],
               ),
             )
-          ],
-        ),
+          : null,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        selectedItemColor: context.colorTheme.onPrimary,
+        unselectedItemColor: context.colorTheme.outline,
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        selectedIconTheme: IconThemeData(size: 28.0),
+        unselectedIconTheme: IconThemeData(size: 24.0),
+        onTap: (value) {
+          setState(() => _currentIndex = value);
+          _pageController.animateToPage(
+            value,
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeInOut,
+          );
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.listCheck),
+            label: "목록",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.calendar),
+            label: "달력",
+          ),
+        ],
       ),
     );
   }
---
+++ lib/ui/home/widget/event_calendar.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:simpletodo/common/theme.dart';
+import 'package:simpletodo/common/tools.dart';
+import 'package:simpletodo/domain/model/todo_model.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class EventCalendar extends StatelessWidget {
+  const EventCalendar({
+    super.key,
+    required this.todos,
+    required this.selectedDay,
+    required this.format,
+    required this.onDaySelected,
+    required this.onFormatChanged,
+  });
+
+  final List<TodoModel> todos;
+
+  final DateTime selectedDay;
+  final CalendarFormat format;
+
+  final ValueChanged<DateTime> onDaySelected;
+  final ValueChanged<CalendarFormat> onFormatChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar<TodoModel>(
+      headerStyle: HeaderStyle(
+        formatButtonVisible: true,
+        titleCentered: true,
+        leftChevronVisible: true,
+        rightChevronVisible: true,
+        headerPadding: const EdgeInsets.only(
+          left: 20.0,
+          right: 20.0,
+          top: 0.0,
+          bottom: 20.0,
+        ),
+        titleTextStyle: context.textTheme.titleSmall!,
+        titleTextFormatter: (date, locale) {
+          return "${date.year}년 ${date.month}월";
+        },
+        formatButtonShowsNext: false,
+      ),
+      daysOfWeekStyle: DaysOfWeekStyle(
+        weekdayStyle: context.textTheme.labelMedium!,
+        weekendStyle: context.textTheme.labelMedium!.copyWith(
+          color: context.colorTheme.error,
+        ),
+      ),
+      calendarStyle: CalendarStyle(
+        todayTextStyle: context.textTheme.bodyMedium!.copyWith(
+          fontWeight: FontWeight.bold,
+        ),
+        holidayTextStyle: context.textTheme.bodyMedium!.copyWith(
+          color: context.colorTheme.error,
+        ),
+        weekendTextStyle: context.textTheme.bodyMedium!.copyWith(
+          color: context.colorTheme.error,
+        ),
+        defaultTextStyle: context.textTheme.bodyMedium!,
+        outsideTextStyle: context.textTheme.bodyMedium!.copyWith(
+          color: context.colorTheme.onSurface.withOpacity(0.5),
+        ),
+        selectedTextStyle: context.textTheme.bodyMedium!.copyWith(
+          color: context.colorTheme.primary,
+          fontWeight: FontWeight.bold,
+        ),
+        todayDecoration: BoxDecoration(
+          color: context.colorTheme.onSurface,
+          shape: BoxShape.circle,
+        ),
+        selectedDecoration: BoxDecoration(
+          color: context.colorTheme.onPrimary,
+          shape: BoxShape.circle,
+        ),
+        markerDecoration: BoxDecoration(
+          color: Colors.blue.shade300,
+          shape: BoxShape.circle,
+          border: Border.all(color: Colors.blue.shade100, width: 1.0),
+        ),
+      ),
+      rangeSelectionMode: RangeSelectionMode.disabled,
+      focusedDay: selectedDay,
+      firstDay: DateTime.now(),
+      lastDay: DateTime.now().add(const Duration(days: 365)),
+      selectedDayPredicate: (day) {
+        return isSameDay(selectedDay, day);
+      },
+      availableCalendarFormats: const {
+        CalendarFormat.month: '1 달',
+        CalendarFormat.twoWeeks: '2 주',
+        CalendarFormat.week: '1 주'
+      },
+      calendarFormat: format,
+      onFormatChanged: onFormatChanged,
+      eventLoader: (day) {
+        return todos.where((e) {
+          if (e.rangeDate != null) {
+            return (e.rangeDate!.start?.isBefore(day) ?? false) &&
+                (e.rangeDate!.end?.isAfter(day) ?? false);
+          } else {
+            return isSameDay(e.dueDate, day);
+          }
+        }).toList();
+      },
+      onDaySelected: (selectedDay, focusedDay) {
+        onDaySelected(selectedDay);
+      },
+    );
+  }
+}
